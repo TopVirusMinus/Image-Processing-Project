8915038d6ea3621e51354c06cb2d1b4ac8e3399e
TopVirusMinus <mwmma5000@gmail.com>
2023-05-04 13:40:03 +0300

    removed pyscript & added flask

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 from flask import Flask, render_template, request
 import base64
-import cv2from flask import Flask, render_template, request
-import base64
 import cv2
 import numpy as np
 
@@ -11,31 +9,17 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/process-image', methods=['POST'])
-def process_image():
-    image_data = request.form.get('image_data')
-    filter_type = request.form.get('filter_type')
+def filter1(image):
+    # Apply filter 1 processing here
+    return image
 
-    image_data = base64.b64decode(image_data.split(',')[1])
-    nparr = np.frombuffer(image_data, np.uint8)
-    image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+def filter2(image):
+    # Apply filter 2 processing here
+    return image
 
-    # Apply your image processing logic here based on filter_type
-
-    retval, buffer = cv2.imencode('.jpg', image)
-    processed_image_base64 = base64.b64encode(buffer).decode('utf-8')
-    return {'processed_image': f'data:image/jpeg;base64,{processed_image_base64}'}
-
-if __name__ == '__main__':
-    app.run(debug=True)
-
-import numpy as np
-
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return render_template('index.html')
+def filter3(image):
+    # Apply filter 3 processing here
+    return image
 
 @app.route('/process-image', methods=['POST'])
 def process_image():
@@ -46,9 +30,14 @@ def process_image():
     nparr = np.frombuffer(image_data, np.uint8)
     image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
 
-    # Apply your image processing logic here based on filter_type
+    if filter_type == 'filter1':
+        processed_image = filter1(image)
+    elif filter_type == 'filter2':
+        processed_image = filter2(image)
+    elif filter_type == 'filter3':
+        processed_image = filter3(image)
 
-    retval, buffer = cv2.imencode('.jpg', image)
+    retval, buffer = cv2.imencode('.jpg', processed_image)
     processed_image_base64 = base64.b64encode(buffer).decode('utf-8')
     return {'processed_image': f'data:image/jpeg;base64,{processed_image_base64}'}
 
